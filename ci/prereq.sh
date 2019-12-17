391584dd46908b2b37e595aaa083567a9cb0cc03
Yuval Kashtan <yuval@redhat.com>
2019-12-17 09:09:23 +0200

    update the prereq.sh to add more buildenv and cri-o

--- ci/prereq.sh
+++ ci/prereq.sh
@@ -2,3 +2,7 @@ yum install -y vim
 yum install -y git
 yum install -y ipmitool
 yum install -y libvirt qemu-kvm mkisofs
+
+dnf install -y go
+dnf groupinstall "Development Tools"
+# the whatever commands to install cri-o (runc, buildah, etc.)
